55516d55daa477abcfce2c73703f1ada4724d637
Suleman <sulemanshaik1@hotmail.com>
2020-03-22 16:12:36 +0530

    dom element concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core'
 @Component(
   {
+    // selector: 'my-app',
+    // template: `
+    //         <div>
+    //           <h1>{{pageHeader}}</h1>
+    //           <img src='https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/{{imagePath}}'/>
+    //           <h1>{{getFullName()}}</h1>
+    //           <span [innerHtml]='innerHtml'></span><br>
+    //           <button disabled='{{isDisable}}'>Click here!!</button><br>
+    //           <button [disabled]='isDisable'>Click here for property binding!!</button>
+    //           <div innerHtml='{{badHtml}}'></div>
+    //           <employee-details></employee-details>
+    //         </div>
+    // `
     selector: 'my-app',
     template: `
             <div>
-              <h1>{{pageHeader}}</h1>
-              <img src='https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/{{imagePath}}'/>
-              <h1>{{getFullName()}}</h1>
-              <span [innerHtml]='innerHtml'></span><br>
-              <button disabled='{{isDisable}}'>Click here!!</button><br>
-              <button [disabled]='isDisable'>Click here for property binding!!</button>
-              <div innerHtml='{{badHtml}}'></div>
-              <employee-details></employee-details>
+              <input type='text' id='inputId' value='Suleman'/>
             </div>
     `
   }
