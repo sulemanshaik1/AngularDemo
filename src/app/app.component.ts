28bac47a0912a55b0fee031ae72a42c810ef489b
Suleman <sulemanshaik1@hotmail.com>
2020-03-22 15:04:36 +0530

    interpolation examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,19 @@ import { Component } from '@angular/core'
     template: `
             <div>
               <h1>{{pageHeader}}</h1>
+              <img src='{{imagePath}}'/>
+              <h1>{{getFullName()}}</h1>
               <employee-details></employee-details>
             </div>
     `
   }
 )
 export class AppComponent {
-  pageHeader: string = "Employee Details"
+  pageHeader: string = "Employee Details";
+  imagePath:string='https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/OBaVg52wtTZ.png';
+  firstName:string='Suleman';
+  lastName:string='Shaik';
+  getFullName():string{
+    return this.firstName+' '+this.lastName;
+  }
 }
