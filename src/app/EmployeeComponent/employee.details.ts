62ff529154fe8b8c2065e43f6380bca0d372a86f
Suleman <sulemanshaik1@hotmail.com>
2020-03-22 12:37:04 +0530

    part7_stylesTopic

--- src/app/EmployeeComponent/employee.details.ts
+++ src/app/EmployeeComponent/employee.details.ts
@@ -1,13 +1,14 @@
-import{Component} from '@angular/core'
+import { Component } from '@angular/core'
 @Component({
-    selector:'employee-details',
-    templateUrl:'../EmployeeComponent/employee.details.html',
+    selector: 'employee-details',
+    templateUrl: '../EmployeeComponent/employee.details.html',
+    styleUrls: ['../EmployeeComponent/employee.details.css']
 }
 
 )
-export class EmployeeDetails{
-    name:string='Suleman';
-    age:number=25;
-    gender:string='Male'
+export class EmployeeDetails {
+    name: string = 'Suleman';
+    age: number = 25;
+    gender: string = 'Male'
 
 }
